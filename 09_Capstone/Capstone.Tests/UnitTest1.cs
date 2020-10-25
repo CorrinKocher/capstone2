edec2db5cfbb7695fe2f24540144c5c8fe62e1d2
Colin <mahan.colin@yahoo.com>
2020-10-25 10:48:52 -0400

    merging

--- 09_Capstone/Capstone.Tests/UnitTest1.cs
+++ 09_Capstone/Capstone.Tests/UnitTest1.cs
@@ -1,13 +1,38 @@
+using Capstone.DAL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 
 namespace Capstone.Tests
 {
     [TestClass]
     public class UnitTest1 : ParentTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void VenueOneReturnsSevenSpaces()
         {
+
+            //Arrange
+            SpaceSQLDAO spaceDao = new SpaceSQLDAO(connectionString);
+            string sql = "SELECT name FROM space WHERE venue_id = 1;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                List<>
+                    }
+
+            while (reader.Read())
+            {
+
+            }
+            //Act
+            spaceDao.DisplayAllSpacesByVenueId("1");
+            //Assert
+            Assert.AreEqual
         }
     }
 }
